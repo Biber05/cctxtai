7c7c830d181cd59cf18bd12aa2e80385df0939a9
Philipp Wolters <philipp.wolters@codecentric.de>
2022-04-24 22:25:08 +0200

    add transformer

---
+++ cctxtai/preprocessing/transform.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+
+from cctxtai.preprocessing.reader import LegalData
+
+
+@dataclass
+class OutputLegalData:
+    id: str
+    slug: str
+    court_name: str
+    type: str
+    content: str
+
+
+def transform(data: [LegalData]) -> [OutputLegalData]:
+    return [OutputLegalData(
+        id=x.id,
+        slug=x.slug,
+        court_name=x.court.name,
+        type=x.type,
+        content=x.content
+    ) for x in data]
--- cctxtai/runner.py
+++ cctxtai/runner.py
@@ -1,6 +1,7 @@
 import click
 
 from cctxtai.preprocessing.reader import DataGenerator
+from cctxtai.preprocessing.transform import transform
 from cctxtai.utils import create_logger
 
 
@@ -9,13 +10,16 @@ from cctxtai.utils import create_logger
 @click.argument("max_lines", default=-1)
 def run(filename: str, max_lines: int):
     log = create_logger(name="runner", log_lvl="DEBUG")
+    data = []
     try:
         data_gen = DataGenerator(source_file_path=filename)
         log.debug(f"Init DataGenerator from {filename}")
         data = data_gen(max_lines=max_lines)
-        log.debug(data[0])
     except FileNotFoundError as e:
         log.error(e)
+
+    result = transform(data)
+
 
 
 if __name__ == '__main__':
